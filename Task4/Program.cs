448517c579e448e8eac31482a640caa5b30de13d
Bekzhan <abildayevbekokk@gmail.com>
2023-02-19 21:37:27 +0600

    vse

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if (a % 7 == 0 && a % 23) //if (a % 161 == 0)
+if (a % 7 == 0 && a % 23 == 0) //if (a % 161 == 0)
 Console.WriteLine("Число имеет в делителях 7 и 23");
